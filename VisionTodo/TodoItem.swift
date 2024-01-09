a027d3780c2eeb37bb0483984eab1c50ad76ab0b
bengHak <qkwl4678@naver.com>
2024-01-09 23:07:59 +0900

    feat: add todo item

---
+++ VisionTodo/TodoItem.swift
@@ -0,0 +1,15 @@
+//
+//  TodoItem.swift
+//  VisionTodo
+//
+//  Created by 고병학 on 1/9/24.
+//
+
+import Foundation
+
+struct TodoItem: Hashable {
+    var title: String
+    var content: String?
+    var createdAt: Date
+    var updatedAT: Date
+}
